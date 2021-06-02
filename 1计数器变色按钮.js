0635622be5a47ea0bdef796cab786efde8aac39e
toooooni <1026534161@qq.com>
2021-06-02 17:47:13 +0800

    counter

---
+++ 1计数器变色按钮.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CountChecker({ count }){
+  const color = count >10 ? "yellow":"pink";
+  return <span style ={{color}}>{count}</span>
+}
+export default function Counter(){
+  const [count,setCount] = React.useState(0);
+  return(
+    <div>
+      <button onClick = {()=>setCount(count +1)}>
+      <CountChecker count = {count}/>
+      </button>
+    </div>
+  )
+}
